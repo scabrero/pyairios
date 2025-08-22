7a45987e7fbc6d119d2f7c83dfcb97a32ea9ac85
Egbert Broerse <dcc2@ebroerse.nl>
2025-08-22 12:25:17 +0200

    move all model info to their files, build prompt on the fly from module name

--- src/pyairios/data_model.py
+++ src/pyairios/data_model.py
@@ -51,84 +51,7 @@ class AiriosDeviceData(AiriosNodeData):
     value_error_status: Result[ValueErrorStatus] | None
 
 
-class VMD02RPS78Data(AiriosDeviceData):
-    """VMD-02RPS78 node data."""
-
-    error_code: Result[VMDErrorCode] | None
-    ventilation_speed: Result[VMDVentilationSpeed] | None
-    override_remaining_time: Result[int] | None
-    exhaust_fan_speed: Result[int] | None
-    supply_fan_speed: Result[int] | None
-    exhaust_fan_rpm: Result[int] | None
-    supply_fan_rpm: Result[int] | None
-    indoor_air_temperature: Result[VMDTemperature] | None
-    outdoor_air_temperature: Result[VMDTemperature] | None
-    exhaust_air_temperature: Result[VMDTemperature] | None
-    supply_air_temperature: Result[VMDTemperature] | None
-    filter_dirty: Result[int] | None
-    filter_remaining_percent: Result[int] | None
-    filter_duration_days: Result[int] | None
-    bypass_position: Result[VMDBypassPosition] | None
-    bypass_mode: Result[VMDBypassMode] | None
-    bypass_status: Result[int] | None
-    defrost: Result[int] | None
-    preheater: Result[VMDHeater] | None
-    postheater: Result[VMDHeater] | None
-    preheater_setpoint: Result[float] | None
-    free_ventilation_setpoint: Result[float] | None
-    free_ventilation_cooling_offset: Result[float] | None
-    frost_protection_preheater_setpoint: Result[float] | None
-    preset_high_fan_speed_supply: Result[int] | None
-    preset_high_fan_speed_exhaust: Result[int] | None
-    preset_medium_fan_speed_supply: Result[int] | None
-    preset_medium_fan_speed_exhaust: Result[int] | None
-    preset_low_fan_speed_supply: Result[int] | None
-    preset_low_fan_speed_exhaust: Result[int] | None
-    preset_standby_fan_speed_supply: Result[int] | None
-    preset_standby_fan_speed_exhaust: Result[int] | None
-
-
-class VMD07RPS13Data(AiriosDeviceData):
-    """
-    VMD-07RPS13 ClimaRad Ventura V1C/V1D/V1X node data.
-    source: ClimaRad Modbus Registers Specs 2024
-    """
-
-    error_code: Result[VMDErrorCode] | None
-    ventilation_speed: Result[VMDVentilationSpeed] | None
-    # override_remaining_time: Result[int] | None
-    exhaust_fan_speed: Result[int] | None
-    supply_fan_speed: Result[int] | None
-    exhaust_fan_rpm: Result[int] | None
-    supply_fan_rpm: Result[int] | None
-    indoor_air_temperature: Result[VMDTemperature] | None
-    outdoor_air_temperature: Result[VMDTemperature] | None
-    exhaust_air_temperature: Result[VMDTemperature] | None
-    supply_air_temperature: Result[VMDTemperature] | None
-    filter_dirty: Result[int] | None
-    filter_remaining_percent: Result[int] | None
-    # filter_duration_days: Result[int] | None
-    bypass_position: Result[VMDBypassPosition] | None
-    # bypass_mode: Result[VMDBypassMode] | None
-    bypass_status: Result[int] | None
-    # defrost: Result[int] | None
-    # preheater: Result[VMDHeater] | None
-    # postheater: Result[VMDHeater] | None
-    # preheater_setpoint: Result[float] | None
-    # free_ventilation_setpoint: Result[float] | None
-    # free_ventilation_cooling_offset: Result[float] | None
-    # frost_protection_preheater_setpoint: Result[float] | None
-    # preset_high_fan_speed_supply: Result[int] | None
-    # preset_high_fan_speed_exhaust: Result[int] | None
-    # preset_medium_fan_speed_supply: Result[int] | None
-    # preset_medium_fan_speed_exhaust: Result[int] | None
-    # preset_low_fan_speed_supply: Result[int] | None
-    # preset_low_fan_speed_exhaust: Result[int] | None
-    # preset_standby_fan_speed_supply: Result[int] | None
-    # preset_standby_fan_speed_exhaust: Result[int] | None
-
-
-##### special devices ######
+##### special devices only, 'normal' node data_models are in their respective module file ######
 
 
 class BRDG02R13Data(AiriosNodeData):
@@ -139,12 +62,6 @@ class BRDG02R13Data(AiriosNodeData):
     rf_load_last_hour: Result[float] | None
     rf_load_current_hour: Result[float] | None
     power_on_time: Result[timedelta] | None
-
-
-class VMN05LM02Data(AiriosDeviceData):
-    """VMN-05LM02 node data."""
-
-    requested_ventilation_speed: Result[VMDRequestedVentilationSpeed] | None
 
 
 @dataclass
