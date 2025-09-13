a8525d1c9736be38a78a2a3c3505ccc323a6142d
Egbert Broerse <dcc2@ebroerse.nl>
2025-09-13 15:18:39 +0200

    test docstr, remove unused

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+"""pyairios minimal pytest suite."""
+
 import logging
 import sys
 
@@ -5,19 +8,23 @@ import pytest
 from mock_serial import MockSerial
 from serial import Serial
 
-# from pyairios.client import AsyncAiriosModbusRtuClient
 from cli import AiriosRootCLI
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(levelname)s - %(message)s")
 
 
-class TestStartPyairios:
-    def test_init_root(self):
+class TestStartPyairiosCli:
+    """
+    CLI tests.
+    """
+
+    def test_init_root(self) -> None:
+        """
+        Test root level init of cli.py.
+        """
         device = MockSerial()
         device.open()
         serial = Serial(device.port)
-
-        stub = device.stub(name="foo", receive_bytes=b"123", send_bytes=b"456")
 
         # init CLI
         cli = AiriosRootCLI()
@@ -29,7 +36,10 @@ class TestStartPyairios:
 
     @pytest.mark.asyncio
     @pytest.mark.timeout(1)
-    async def test_init(self):
+    async def test_init(self) -> None:
+        """
+        Test cli.py serial connect on mocked port.
+        """
         device = MockSerial()
         device.open()
         serial = Serial(device.port)
@@ -39,7 +49,7 @@ class TestStartPyairios:
         # init CLI
         cli = AiriosRootCLI()
         try:
-            await cli.do_connect_rtu(serial)
+            await cli.do_connect_rtu(str(serial))
             # timeout here, but no error
             # TODO(eb): mock serial port so test can run, see pymodbus tests
         except TimeoutError:
