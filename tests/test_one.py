2776be91cf6ce67aa196f88ba15aa97b70ebcb4a
Egbert Broerse <dcc2@ebroerse.nl>
2025-09-22 20:42:16 +0200

    roll back tests to master

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,77 +1,39 @@
 #!/usr/bin/env python3
 """pyairios minimal pytest suite."""
-# compare to pymodbus/test/client/test_client_sync.py TestSyncClientSerial
 
 import logging
-import pytest
 import sys
 
+from mock_serial import MockSerial
+from serial import Serial
+
 from cli import AiriosRootCLI
-from pyairios import Airios, AiriosRtuTransport
-from pyairios.exceptions import AiriosConnectionException
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(levelname)s - %(message)s")
 
 
-class TestStartPyairiosCli:
+class TestStartPyairios:  # pylint: disable=too-few-public-methods
     """
-    CLI tests.
+    pyairios tests.
     """
 
-    def test_cli_root_init(self) -> None:
+    def test_init_cli_root(self) -> None:
         """
-        Test cli.py root level init.
+        Test root level init of cli.py.
         """
+        device = MockSerial()
+        device.open()
+        serial = Serial(device.port)
 
         # init CLI
         cli = AiriosRootCLI()
         assert cli, "no CLI"
 
-    @pytest.mark.asyncio
-    async def test_cli_connect(self) -> None:
-        """
-        Test cli.py serial connect on null port.
-        """
+        # break down
+        serial.close()
+        device.close()
 
-        # init CLI
-        cli = AiriosRootCLI()
-        # connect
-        await cli.do_connect_rtu("/dev/null")
+    # TODO(eb): mock serial port so connect + next cli levels can run
+    #  see pymodbus simulator + their tests
 
-        assert cli.client, "no client"
-
-
-class TestStartPyairiosApi:
-    """
-    Airios api tests.
-    """
-
-    @pytest.mark.asyncio
-    async def test_api_init(self) -> None:
-        """
-        Test pyairios api serial init.
-        """
-
-        transport = AiriosRtuTransport("/dev/null")
-
-        # init api
-        api = Airios(transport)
-        assert api, "no api"
-
-    @pytest.mark.asyncio
-    async def test_api_connect(self) -> None:
-        """
-        Test pyairios api serial connect.
-        """
-
-        transport = AiriosRtuTransport("/dev/null")
-        api = Airios(transport)
-
-        # try to connect
-        try:
-            await api.connect()
-        except AiriosConnectionException:
-            pass
-        else:
-            raise AssertionError("Expected AiriosConnectionException")
-        # api.close()
+    # TODO(eb): add Airios api init test
