5b15197b2217841cbd372fd5b520c0f233313e08
Egbert Broerse <dcc2@ebroerse.nl>
2025-09-26 21:50:20 +0200

    rollback dataclass default int

--- src/pyairios/models/vmd_base.py
+++ src/pyairios/models/vmd_base.py
@@ -19,9 +19,9 @@ class VMDPresetFansSpeeds:
     """Preset fan speeds."""
 
     # this must load from vmd_base to prevent None error
-    exhaust_fan_speed: Result[int] = field(default=Result(-1))
+    exhaust_fan_speed: Result[int] = field()
     """Exhaust fan speed (%)"""
-    supply_fan_speed: Result[int] = field(default=Result(-1))
+    supply_fan_speed: Result[int] = field()
     """Supply fan speed (%)"""
 
     def __post_init__(self):
